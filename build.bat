a8fa36991a2569f3d6a7e2d8f10281fc78cff2b3
MatheusZickuhr <matheuszickuhr97@gmail.com>
2025-11-01 15:19:45 -0300

    moved go files to src folder

---
+++ build.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Compila o projeto Go na pasta src e gera javaman.exe na raiz
+go build -o javaman.exe ./src
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+REM Executa o projeto Go diretamente da pasta src
+
+go run ./src
